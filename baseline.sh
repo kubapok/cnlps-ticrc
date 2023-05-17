a8005679454a2a834e640c1fffd9f34dbbdda2ab
Jakub Pokrywka <jakubpokrywka@gmail.com>
2023-05-17 11:41:16 +0200

    update baseline

--- baseline.sh
+++ baseline.sh
@@ -1,6 +1,7 @@
 # Run the baseline
 python baseline.py dev-0
 python baseline.py test-A
+python baseline.py test-B
 
 # Evaluate the baseline results on the dev-0 dataset
 wget https://gonito.net/get/bin/geval
